a0a2dc1b83053dfbd2ee3c7541d9c72d7a4f61a4
LuisCuadradoGuerra <lcuadradoguerra@gmail.com>
2025-11-13 10:32:44 +0100

    Error view and router
    Good practice in router added like lazy loading, name paths and meta attributes.
    In addition, a new path and view was implemented for not usual paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,41 @@
-import CardView from '@/views/CardView.vue'
-import SelectionView from '@/views/SelectionView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: CardView },
-    { path: '/selection', component: SelectionView },
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/CardView.vue'),
+      meta: {
+        requiresAuth: false,
+        title: 'Home',
+      },
+    },
+    {
+      path: '/selection',
+      name: 'Selection',
+      component: () => import('@/views/SelectionView.vue'),
+      meta: {
+        requiresAuth: false,
+        title: 'Selection',
+      },
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('@/views/NotFoundView.vue'),
+      meta: {
+        requiresAuth: false,
+        title: 'Alguien se ha hecho caquita...',
+      },
+    },
   ],
+})
+
+router.beforeEach((to, _, next) => {
+  document.title = (to.meta.title as string) || 'Mi App Vue'
+  next()
 })
 
 export default router
